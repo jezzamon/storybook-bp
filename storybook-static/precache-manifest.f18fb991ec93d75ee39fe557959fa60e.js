a2ffab0685ff9a376ae7beb799b6a998e35a1f6d
jezzamondev <jezzamondev@gmail.com>
2020-02-24 08:42:04 -0500

    addon examples

---
+++ storybook-static/precache-manifest.f18fb991ec93d75ee39fe557959fa60e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7faa13e8ff796d60ce9c03f45bde3b46",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.c55bcc185c7956ec027f.bundle.js"
+  },
+  {
+    "url": "runtime~main.c55bcc185c7956ec027f.bundle.js"
+  },
+  {
+    "revision": "20799755087b21437754",
+    "url": "static/css/main.9ec6dc68.chunk.css"
+  },
+  {
+    "url": "vendors~main.c55bcc185c7956ec027f.bundle.js"
+  },
+  {
+    "url": "vendors~main.c55bcc185c7956ec027f.bundle.js.LICENSE.txt"
+  }
+]);
